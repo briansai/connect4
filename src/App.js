548d92eca5b6b9bd7038df373ca3515c82572ef9
Brian Sai <brianwsai@gmail.com>
2021-01-06 19:31:14 -0800

    Initial visual representation of the game board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import './App.css';
+import React, { useState } from 'react';
+import Board from './classes/Board';
+import './App.scss';
 
 function App() {
-  return <div className="App">Connect 4</div>;
+  const [] = useState(false);
+  const [board, setBoard] = useState(new Board());
+
+  return (
+    <div className="App">
+      <table className="board">
+        {board.state.map((row, index) => {
+          return (
+            <tr key={index} className="row">
+              {row.map(() => (
+                <td className="slot">
+                  <div className="disc"></div>
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
 
 export default App;
