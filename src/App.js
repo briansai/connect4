cd7837c40ba5a79f3734681dbbd986d12198f3d8
Brian Sai <brianwsai@gmail.com>
2021-01-07 17:06:12 -0800

    Set disc to position of player if the slot is available

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Board from './classes/Board';
 import './App.scss';
 
 function App() {
-  const [] = useState(false);
-  const [board, setBoard] = useState(new Board());
+  const board = new Board();
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
+  board.setDisc(3, 1);
 
+  board.setDisc(1, 2);
+
+  console.log(board);
   return (
     <div className="App">
       <table className="board">
-        {board.state.map((row, index) => {
-          return (
-            <tr key={index} className="row">
-              {row.map(() => (
-                <td className="slot">
-                  <div className="disc"></div>
-                </td>
-              ))}
-            </tr>
-          );
-        })}
+        <tbody>
+          {board.state.map((row, index) => {
+            return (
+              <tr key={index} className="row">
+                {row.map(() => (
+                  <td className="slot">
+                    <div className="disc"></div>
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
